4b7b7ac2ea9ae7be39a5363a4c8653458843ebc0
diego <diegoteixeirapereira@hotmail.com>
2021-11-19 14:58:21 -0300

    criação dos arquivos de construção do recyclerView.

--- app/src/main/java/br/java/meuapprecyclerview/MainActivity.java
+++ app/src/main/java/br/java/meuapprecyclerview/MainActivity.java
@@ -6,9 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private List<Filme> listaFilmes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +21,30 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
 
+        this.criarFilmes();
+
+        // Adapter
+        Adapter adapter = new Adapter(listaFilmes);
+
         // LayoutManger
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
-
-        // Adapter
-
-
+        recyclerView.setAdapter(adapter);
         // Dataset
+    }
 
+    public void criarFilmes() {
+        Filme filme = new Filme("O Homem Aranha", "genero", "ano");
+        listaFilmes.add(filme);
+
+        filme = new Filme("Capitão América","ação","2015");
+        listaFilmes.add(filme);
+
+        filme = new Filme("A Lenda","drama","2011");
+        listaFilmes.add(filme);
+
+        filme = new Filme("Lagoa Azul","drama","1990");
+        listaFilmes.add(filme);
     }
 }
