77dfe6153830e7743a17641edd312989986aefd9
diego <diegoteixeirapereira@hotmail.com>
2021-11-19 14:29:01 -0300

    criação do app android com exemplo de recyclerView

---
+++ app/src/main/java/br/java/meuapprecyclerview/MainActivity.java
@@ -0,0 +1,31 @@
+package br.java.meuapprecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        // LayoutManger
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        // Adapter
+
+
+        // Dataset
+
+    }
+}
